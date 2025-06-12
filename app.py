95af4063639049746299a396cc0ea127dd2ee69d
tiara <diana_lilo2594@hotmail.com>
2025-06-12 12:50:28 -0700

    Updating CI/CD to include Deployment

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, jsonify
 app = Flask (__name__)
 @app.route('/health', methods=['GET']) 
 def health_check():
-    return jsonify({'status':'OK'})
+    return jsonify({'status':'UP'})
 
 if __name__ == '__main__':
     app.run(debug=True)
